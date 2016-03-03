6f32fc64e22c0427f2e13f698489f0fb550e74a8
Carden Bagwell <cardenbag@gmail.com>
2016-03-03 14:04:43 -0500

    Reformatted some code for readability.

--- src/tracker/tracker.py
+++ src/tracker/tracker.py
@@ -8,18 +8,27 @@ if __name__ == '__main__':
     outFileName = sys.argv[2]
     ##CODEC = cv2.CV_FOURCC('D', 'I', 'V', 'X') # MPEG-4 = MPEG-1A
     videoIn = cv2.VideoCapture(inVideoName)
-    videoOut = cv2.VideoWriter(outFileName, cv2.VideoWriter_fourcc('m', 'p', 'e', 'g'), int(videoIn.get(cv2.CAP_PROP_FRAME_COUNT)), (int(videoIn.get(cv2.CAP_PROP_FRAME_WIDTH)), int(videoIn.get(cv2.CAP_PROP_FRAME_HEIGHT))))
+    videoOut = cv2.VideoWriter(outFileName,
+                               cv2.VideoWriter_fourcc('m', 'p', 'e', 'g'),
+                               int(videoIn.get(cv2.CAP_PROP_FRAME_COUNT)),
+                               (int(videoIn.get(cv2.CAP_PROP_FRAME_WIDTH)),
+                                int(videoIn.get(cv2.CAP_PROP_FRAME_HEIGHT))))
+
     if not videoIn.isOpened():
         print("Error opening input video" + inVideoName)
     if not videoOut.isOpened():
         print("Error opening output video" + outFileName)
+
     while(videoIn.grab()):
         ret, inFrame = videoIn.retrieve()
         imageScale = .66
         # inFrame = cv2.resize(inFrame, (0, 0), inFrame, imageScale, imageScale)
         blur = cv2.GaussianBlur(inFrame, (7, 7), 3)
         gray = cv2.cvtColor(blur, cv2.COLOR_BGR2GRAY)
-        binary = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 31, 40) ## + cv2.THRESH_OTSU)
+        binary = cv2.adaptiveThreshold(gray, 255,
+                                       cv2.ADAPTIVE_THRESH_MEAN_C,
+                                       cv2.THRESH_BINARY_INV, 31, 40)
+                                       ## + cv2.THRESH_OTSU)
 
         im, contours, heirarchy = cv2.findContours(binary, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
         # print(contours[1])
